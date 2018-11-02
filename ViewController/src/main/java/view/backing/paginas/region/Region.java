aae824cc38eefd10f8ebd82e2c5e0fa5981ebfdf
Ricardo Gentil <rgentil2112@gmail.com>
2018-11-02 09:05:04 -0300

    Se agrega vinculo entre provincia  y región

---
+++ ViewController/src/main/java/view/backing/paginas/region/Region.java
@@ -0,0 +1,247 @@
+package main.java.view.backing.paginas.region;
+
+import oracle.adf.view.faces.bi.component.chart.UIBarChart;
+import oracle.adf.view.faces.bi.component.chart.UIDataItem;
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.data.RichListItem;
+import oracle.adf.view.rich.component.rich.data.RichListView;
+import oracle.adf.view.rich.component.rich.data.RichTable;
+import oracle.adf.view.rich.component.rich.input.RichInputDate;
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+import oracle.adf.view.rich.component.rich.layout.RichGridCell;
+import oracle.adf.view.rich.component.rich.layout.RichGridRow;
+import oracle.adf.view.rich.component.rich.layout.RichPanelBox;
+import oracle.adf.view.rich.component.rich.layout.RichPanelDashboard;
+import oracle.adf.view.rich.component.rich.layout.RichPanelFormLayout;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGridLayout;
+import oracle.adf.view.rich.component.rich.output.RichMessages;
+import oracle.adf.view.rich.component.rich.output.RichOutputFormatted;
+
+public class Region {
+    private RichForm f1;
+    private RichDocument d1;
+    private RichMessages m1;
+    private RichListView lv1;
+    private RichListItem li1;
+    private RichPanelGridLayout pgl1;
+    private RichGridRow gr1;
+    private RichGridCell gc1;
+    private RichOutputFormatted of1;
+    private RichGridCell gc2;
+    private RichOutputFormatted of2;
+    private RichPanelDashboard pd1;
+    private RichPanelBox pb1;
+    private RichPanelBox pb2;
+    private RichPanelBox pb3;
+    private RichPanelBox pb4;
+    private RichTable t1;
+    private RichPanelFormLayout pfl1;
+    private RichInputText it1;
+    private RichInputText it2;
+    private RichInputText it3;
+    private RichInputDate id1;
+    private RichInputText it4;
+    private UIBarChart barChart1;
+    private UIDataItem di1;
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+
+    public void setM1(RichMessages m1) {
+        this.m1 = m1;
+    }
+
+    public RichMessages getM1() {
+        return m1;
+    }
+
+    public void setLv1(RichListView lv1) {
+        this.lv1 = lv1;
+    }
+
+    public RichListView getLv1() {
+        return lv1;
+    }
+
+    public void setLi1(RichListItem li1) {
+        this.li1 = li1;
+    }
+
+    public RichListItem getLi1() {
+        return li1;
+    }
+
+    public void setPgl1(RichPanelGridLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGridLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setGr1(RichGridRow gr1) {
+        this.gr1 = gr1;
+    }
+
+    public RichGridRow getGr1() {
+        return gr1;
+    }
+
+    public void setGc1(RichGridCell gc1) {
+        this.gc1 = gc1;
+    }
+
+    public RichGridCell getGc1() {
+        return gc1;
+    }
+
+    public void setOf1(RichOutputFormatted of1) {
+        this.of1 = of1;
+    }
+
+    public RichOutputFormatted getOf1() {
+        return of1;
+    }
+
+    public void setGc2(RichGridCell gc2) {
+        this.gc2 = gc2;
+    }
+
+    public RichGridCell getGc2() {
+        return gc2;
+    }
+
+    public void setOf2(RichOutputFormatted of2) {
+        this.of2 = of2;
+    }
+
+    public RichOutputFormatted getOf2() {
+        return of2;
+    }
+
+    public void setPd1(RichPanelDashboard pd1) {
+        this.pd1 = pd1;
+    }
+
+    public RichPanelDashboard getPd1() {
+        return pd1;
+    }
+
+    public void setPb1(RichPanelBox pb1) {
+        this.pb1 = pb1;
+    }
+
+    public RichPanelBox getPb1() {
+        return pb1;
+    }
+
+    public void setPb2(RichPanelBox pb2) {
+        this.pb2 = pb2;
+    }
+
+    public RichPanelBox getPb2() {
+        return pb2;
+    }
+
+    public void setPb3(RichPanelBox pb3) {
+        this.pb3 = pb3;
+    }
+
+    public RichPanelBox getPb3() {
+        return pb3;
+    }
+
+    public void setPb4(RichPanelBox pb4) {
+        this.pb4 = pb4;
+    }
+
+    public RichPanelBox getPb4() {
+        return pb4;
+    }
+
+    public void setT1(RichTable t1) {
+        this.t1 = t1;
+    }
+
+    public RichTable getT1() {
+        return t1;
+    }
+
+    public void setPfl1(RichPanelFormLayout pfl1) {
+        this.pfl1 = pfl1;
+    }
+
+    public RichPanelFormLayout getPfl1() {
+        return pfl1;
+    }
+
+    public void setIt1(RichInputText it1) {
+        this.it1 = it1;
+    }
+
+    public RichInputText getIt1() {
+        return it1;
+    }
+
+    public void setIt2(RichInputText it2) {
+        this.it2 = it2;
+    }
+
+    public RichInputText getIt2() {
+        return it2;
+    }
+
+    public void setIt3(RichInputText it3) {
+        this.it3 = it3;
+    }
+
+    public RichInputText getIt3() {
+        return it3;
+    }
+
+    public void setId1(RichInputDate id1) {
+        this.id1 = id1;
+    }
+
+    public RichInputDate getId1() {
+        return id1;
+    }
+
+    public void setIt4(RichInputText it4) {
+        this.it4 = it4;
+    }
+
+    public RichInputText getIt4() {
+        return it4;
+    }
+
+    public void setBarChart1(UIBarChart barChart1) {
+        this.barChart1 = barChart1;
+    }
+
+    public UIBarChart getBarChart1() {
+        return barChart1;
+    }
+
+    public void setDi1(UIDataItem di1) {
+        this.di1 = di1;
+    }
+
+    public UIDataItem getDi1() {
+        return di1;
+    }
+}
